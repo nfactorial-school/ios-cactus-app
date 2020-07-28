fecf5af2d73e3a41cef57886d47db2d3f2ef7360
Timur Tabynbayev <timur.tabynbayev@gmail.com>
2020-07-28 17:30:16 +0600

    Added onboarding screen

--- Cactus/OnboardingViewController.swift
+++ Cactus/OnboardingViewController.swift
@@ -9,13 +9,44 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    @IBOutlet var textLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nextButton: UIButton!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    let titles = ["Welcome\nto the\nCactus app!",
+                  "Choose\ncactus\nto plant.",
+                  "Stay focused\n& productive!"]
+    let imageNames = ["onboarding_welcome",
+                      "onboarding_choose",
+                      "onboarding_stay"]
+    var currentIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nextButton.layer.cornerRadius = 14
+        nextButton.layer.masksToBounds = true
+        
+        updateInterface()
     }
     
     @IBAction func didTapNextButton() {
-        performSegue(withIdentifier: "showSession", sender: nil)
+        if currentIndex == 2 {
+            performSegue(withIdentifier: "showSession", sender: nil)
+        } else {
+            currentIndex += 1
+            pageControl.currentPage = currentIndex
+            updateInterface()
+        }
+    }
+    
+    func updateInterface() {
+        textLabel.text = titles[currentIndex]
+        imageView.image = UIImage(named: imageNames[currentIndex])
+        
+        if currentIndex == 2 {
+            nextButton.setTitle("Start", for: .normal)
+        }
     }
 }
