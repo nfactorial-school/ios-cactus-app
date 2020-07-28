dde8719a6e0b96aa6ba5af971c76feda475eabf1
Timur Tabynbayev <timur.tabynbayev@gmail.com>
2020-07-28 09:17:57 +0600

    Initial Commit

---
+++ Cactus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cactus
+//
+//  Created by Тимур Табынбаев on 7/28/20.
+//  Copyright © 2020 Тимур Табынбаев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
