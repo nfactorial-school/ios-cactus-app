75056bc9e685e186d292a0ad3cebcc65c5f39e32
Timur Tabynbayev <timur.tabynbayev@gmail.com>
2020-07-28 15:51:36 +0600

    corner radios for plant button

--- Cactus/SessionViewController.swift
+++ Cactus/SessionViewController.swift
@@ -10,12 +10,16 @@ import UIKit
 
 class SessionViewController: UIViewController {
     @IBOutlet var cactusImageView: UIImageView!
-    
+    @IBOutlet var plantButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         cactusImageView.layer.cornerRadius = cactusImageView.frame.width / 2
         cactusImageView.layer.masksToBounds = true
+        
+        plantButton.layer.cornerRadius = 14
+        plantButton.layer.masksToBounds = true
     }
     
     @IBAction func didTapPlantButton() {
